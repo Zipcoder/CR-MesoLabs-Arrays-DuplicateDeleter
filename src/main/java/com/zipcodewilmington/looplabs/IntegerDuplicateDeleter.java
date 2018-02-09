ab8125f29b171dc04ef266ecbc7af159f6fc2d93
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-02-09 17:21:56 -0500

    saving because it compiles

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,28 @@
 package com.zipcodewilmington.looplabs;
-
+import java.util.Arrays;
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter (Integer[] intArray) {
+        super(intArray);
+    }
+
+    public Integer[] removeDuplicates  (int maxNumberOfDuplications){
+       // System.out.println(Arrays.toString(intArray));
+        System.out.println(Arrays.toString(this.array));
+        Integer[] test = new Integer[1];
+        return test;
+
+    }
+
+    //@Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications){
+        System.out.println(Arrays.toString(this.array));
+        Integer[] test = new Integer[1];
+        return test;
+    }
+
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,28 @@
 package com.zipcodewilmington.looplabs;
+import java.util.Arrays;
 
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+    public StringDuplicateDeleter (String[] stringArray) {
+        super(stringArray);
+    }
+
+    public String[] removeDuplicates  (int maxNumberOfDuplications){
+        System.out.println(Arrays.toString(this.array));
+        //int [] test = new int[];
+        return null;
+
+    }
+
+    //@Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications){
+
+        return null;
+    }
+
+
 }
