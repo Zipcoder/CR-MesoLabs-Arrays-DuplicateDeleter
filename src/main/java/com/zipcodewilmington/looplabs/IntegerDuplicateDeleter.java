85e4e2c6c14c860d51d76cc4496a1ed6aa5ff0bd
Anthony Jordan <anthonyjordan@zipcoders-MacBook-Pro-10.local>
2018-02-09 16:46:08 -0500

    Finished project

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,69 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        StringBuilder removedString = new StringBuilder();
+        int dupeCount = 0;
+        for (int count = 0; count < this.array.length; count++) {
+            for (int count2 = 0; count2 <this.array.length; count2++) {
+                if (this.array[count].equals(this.array[count2])) {
+                    dupeCount++;
+                }
+            }
+            if (!(dupeCount == exactNumberOfDuplications)) {
+                removedString.append(this.array[count]);
+            }
+            dupeCount = 0;
+
+        }
+        String[] removedStringArray = removedString.toString().split("");
+        if (!removedStringArray[0].equals("")) {
+
+            Integer[] intArray = new Integer[removedStringArray.length];
+            for (int count = 0; count < intArray.length; count++) {
+                intArray[count] = Integer.parseInt(removedStringArray[count]);
+            }
+
+            return intArray;
+        } else {
+            Integer[] empty = new Integer[0];
+            return empty;
+        }
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        StringBuilder removedString = new StringBuilder();
+        int dupeCount = 0;
+        for (int count = 0; count < this.array.length; count++) {
+            for (int count2 = 0; count2 <this.array.length; count2++) {
+                if (this.array[count].equals(this.array[count2])) {
+                    dupeCount++;
+                }
+            }
+            if (!(dupeCount >= maxNumberOfDuplications)) {
+                removedString.append(this.array[count]);
+            }
+            dupeCount = 0;
+
+        }
+        String[] removedStringArray = removedString.toString().split("");
+        if (!removedStringArray[0].equals("")) {
+
+            Integer[] intArray = new Integer[removedStringArray.length];
+            for (int count = 0; count < intArray.length; count++) {
+                intArray[count] = Integer.parseInt(removedStringArray[count]);
+            }
+
+            return intArray;
+        } else {
+            Integer[] empty = new Integer[0];
+            return empty;
+        }
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -5,4 +5,59 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] stringArray) {
+        super(stringArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        StringBuilder removedString = new StringBuilder();
+        int dupeCount = 0;
+        for (int count = 0; count < this.array.length; count++) {
+            for (int count2 = 0; count2 < this.array.length; count2++) {
+                if (this.array[count].equals(this.array[count2])) {
+                    dupeCount++;
+                }
+            }
+            if (!(dupeCount >= maxNumberOfDuplications)) {
+                removedString.append(this.array[count]);
+                removedString.append(",");
+            }
+            dupeCount = 0;
+
+        }
+        String[] removedStringArray = removedString.toString().split(",");
+        if (!removedStringArray[0].equals("")) {
+            return removedStringArray;
+        } else {
+            String[] empty = new String[0];
+            return empty;
+        }
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        StringBuilder removedString = new StringBuilder();
+        int dupeCount = 0;
+        for (int count = 0; count < this.array.length; count++) {
+            for (int count2 = 0; count2 < this.array.length; count2++) {
+                if (this.array[count].equals(this.array[count2])) {
+                    dupeCount++;
+                }
+            }
+            if (!(dupeCount == exactNumberOfDuplications)) {
+                removedString.append(this.array[count]);
+                removedString.append(",");
+            }
+            dupeCount = 0;
+
+        }
+        String[] removedStringArray = removedString.toString().split(",");
+        if (!removedStringArray[0].equals("")) {
+            return removedStringArray;
+        } else {
+            String[] empty = new String[0];
+            return empty;
+        }
+    }
 }
