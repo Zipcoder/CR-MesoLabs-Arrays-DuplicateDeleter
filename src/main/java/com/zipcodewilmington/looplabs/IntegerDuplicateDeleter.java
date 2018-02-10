477400ba47e6f9f2051fddcec2879f024639f70e
Daniel Horowitz <danielhorowitz@zipcoders-MacBook-Pro-80.local>
2018-02-09 19:00:10 -0500

    i am sorry for what you are about to witness Leon

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,42 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+
+        //String without = " ";
+
+
+        return new Integer[0];
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+
+        String without = "";
+        int counter = 0;
+
+        for (int x = 0; x < this.array.length; x ++) {
+            for(int y = 0; y < this.array.length; y++) {
+                if (this.array[x].equals(this.array[y])) {
+                    counter++;
+                }
+            }
+            if (counter != exactNumberOfDuplications) {
+                without += this.array[x] + " ";
+            }
+            counter = 0;
+        }
+        String[] last = without.split(" ");
+        Integer[] done = new Integer[last.length];
+            for (int i = 0; i < last.length; i++) {
+                done[i] = Integer.parseInt(last[i]);
+            }
+        System.out.println(done);
+        return done;
+    }
 }
