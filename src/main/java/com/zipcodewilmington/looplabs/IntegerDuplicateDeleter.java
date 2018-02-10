d3a2d494ff9e251a8a7fea52c0ff56be92b8ac36
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-02-09 19:53:49 -0500

    maybe not the best way

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,50 @@
 package com.zipcodewilmington.looplabs;
 
+
 /**
  * Created by leon on 1/29/18.
+ *
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        return new Integer[0];
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+
+        String noTrips = "";
+        int occurences = 0;
+
+        for (int i = 0; i < this.array.length; i++) {
+
+            for (int k = 0; k < this.array.length; k++) {
+
+                if (this.array[k] == (this.array[i])) {
+                    occurences++;
+                }
+            }
+
+            if (occurences != exactNumberOfDuplications) {
+                noTrips += this.array[i] + " ";
+            }
+            occurences = 0;
+        }
+
+        String[] noTripsArray = noTrips.split(" ");
+        Integer[] noTripsIntArray = new Integer[noTripsArray.length];
+
+        for (int i = 0; i < noTripsArray.length; i++) {
+            noTripsIntArray[i] = Integer.parseInt(noTripsArray[i]);
+        }
+
+        return noTripsIntArray;
+    }
 }
