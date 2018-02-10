e99efe0a46a2095532b445db9405ec5c771902a9
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-02-10 11:08:20 -0500

    it works though

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -14,7 +14,43 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
 
     @Override
     public Integer[] removeDuplicates(int maxNumberOfDuplications) {
-        return new Integer[0];
+
+        String noDupes = "";
+        int occurences = 0;
+        int countAdded = 0;
+
+        for (int i = 0; i < this.array.length; i++) {
+
+            for (int k = 0; k < this.array.length; k++) {
+
+                if (this.array[k] == (this.array[i])) {
+                    occurences++;
+                }
+            }
+
+            if (occurences < maxNumberOfDuplications) {
+                noDupes += this.array[i] + " ";
+                countAdded++;
+            }
+            occurences = 0;
+        }
+
+        String[] dupeFree = noDupes.split(" ");
+        //System.out.println(dupeFree.length);
+
+        if (countAdded > 0) {
+            Integer[] dupeFreeIntArray = new Integer[dupeFree.length];
+
+            for (int i = 0; i < dupeFree.length; i++) {
+                dupeFreeIntArray[i] = Integer.parseInt(dupeFree[i]);
+            }
+            return dupeFreeIntArray;
+        } else {
+            Integer[] emptyIntArray = new Integer[0];
+            return emptyIntArray;
+        }
+
+
     }
 
     @Override
