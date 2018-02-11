f25476f6b1e70c07bd17e23bc1bb2a65a9ece6bd
Luis Romero <luisromero@zipcoders-MacBook-Pro-47.local>
2018-02-11 12:59:36 -0500

    first commit 12:59pm on 0211

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -5,4 +5,78 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+    private int lastIndex;
+    private static final String[] ZERO_LENGTH_STRING_ARRAY = new String[0];
+
+    public StringDuplicateDeleter(String[] stringArray) {
+        super(stringArray);
+    }
+
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        //String[] testArr = new String[2];
+        //return testArr;
+
+        String[] inputArray = this.array;
+        lastIndex = inputArray.length - 1;
+
+        StringBuilder sb = new StringBuilder();
+
+        int frequency = 1;
+        int times = maxNumberOfDuplications;
+        for (int i = 0; i < inputArray.length; i++) {
+            if ((i != lastIndex) && (inputArray[i] == inputArray[i + 1])) {
+                frequency++;
+            } else {
+                if (!(frequency >= times)) {
+                    while (frequency >= 1) {
+                        sb.append(inputArray[i] + ",");
+                        frequency--;
+                    }
+                }
+                frequency = 1;
+            }
+        }
+
+        if (sb == null || sb.toString().equals("")) {
+            return ZERO_LENGTH_STRING_ARRAY;
+        } else {
+            String[] strArr = sb.toString().split(",");
+            return strArr;
+        }
+    }
+
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        //String[] testArr = new String[2];
+        //return testArr;
+
+        String[] inputArray = this.array;
+        lastIndex = inputArray.length - 1;
+
+        StringBuilder sb = new StringBuilder();
+
+        int frequency = 1;
+        int times = exactNumberOfDuplications;
+        for (int i = 0; i < inputArray.length; i++) {
+            if ((i != lastIndex) && (inputArray[i] == inputArray[i + 1])) {
+                frequency++;
+            } else {
+                if (frequency != times) {
+                    while (frequency >= 1) {
+                        sb.append(inputArray[i] + ",");
+                        frequency--;
+                    }
+                }
+                frequency = 1;
+            }
+        }
+
+        if (sb == null || sb.toString().equals("")) {
+            return ZERO_LENGTH_STRING_ARRAY;
+        } else {
+            String[] strArr = sb.toString().split(",");
+            return strArr;
+        }
+    }
+
 }
