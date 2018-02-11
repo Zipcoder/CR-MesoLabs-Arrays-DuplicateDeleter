480de475d62882e10af47c97a80d59efd72536fe
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-02-10 19:39:39 -0500

    fixed unintended formatting issue in file

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -4,42 +4,42 @@ import java.util.Arrays;
 
 /**
  * Created by leon on 1/29/18.
+ *
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 
 
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
 
- public IntegerDuplicateDeleter(Integer[] intArray) {
- super(intArray);
- }
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
 
- @Override
- public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
 
- Integer [] resultArray = new Integer[0];
- for(int i = 0; i < this.array.length; i++){
- if(getNumberOfOccurrences(this.array, this.array[i]) >= maxNumberOfDuplications){
- resultArray = Arrays.copyOf(resultArray, resultArray.length+1);
- }
- }
+        Integer[] resultArray = new Integer[0];
+        for (int i = 0; i < this.array.length; i++) {
+            if (getNumberOfOccurrences(this.array, this.array[i]) >= maxNumberOfDuplications) {
+                resultArray = Arrays.copyOf(resultArray, resultArray.length + 1);
+            }
+        }
 
- return resultArray;
- }
+        return resultArray;
+    }
 
- @Override
- public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
 
- Integer [] resultArray = new Integer[0];
- for(int i = 0; i < this.array.length; i++){
- if(getNumberOfOccurrences(this.array, this.array[i]) >= exactNumberOfDuplications){
- resultArray = Arrays.copyOf(resultArray, resultArray.length+1);
- }
- }
+        Integer[] resultArray = new Integer[0];
+        for (int i = 0; i < this.array.length; i++) {
+            if (getNumberOfOccurrences(this.array, this.array[i]) >= exactNumberOfDuplications) {
+                resultArray = Arrays.copyOf(resultArray, resultArray.length + 1);
+            }
+        }
 
- return resultArray;
- }
-
+        return resultArray;
+    }
 
 
 }
