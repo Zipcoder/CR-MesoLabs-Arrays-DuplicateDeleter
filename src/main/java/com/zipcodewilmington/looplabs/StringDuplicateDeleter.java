6d97b7f7d77671daf759cff89f2850ff46603348
Joshua Wurdemann <joshuawurdemann@zipcoders-MacBook-Pro-9.local>
2018-02-10 19:33:09 -0500

    close to solving string

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -16,8 +18,59 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
         return new String[0];
     }
 
+
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+
+        return removeNumber(exactNumberOfDuplications);
+    }
+
+    public String[] removeNumber(int max) {
+        //StringBuilder r = new StringBuilder();
+        String[] result = new String[0];
+
+        String[] temp = new String[1];
+        int sum = 0;
+
+
+        for (int i = 0; i < this.array.length; i++) {
+             temp[0] = this.array[i];
+
+            if (countStrings(temp) == max) {
+
+                result = Arrays.copyOf(result, sum + 1);
+                result[sum] = temp[0];
+                sum++;
+            }
+        }
+        return result;
+    }
+
+    public int countStrings(String[] s) {
+        int count = 0;
+
+        for (int i = 0; i < this.array.length; i++) {
+            if (this.array[i].equals(s[0])) {
+                count++;
+
+            }
+        }
+        printInt(count);
+        return count;
+    }
+
+    /**
+     * @param something prints each elment in the array
+     */
+    public void printMethod(String[] something) {
+        for (String i : something) {
+
+            System.out.println(i.toString());
+        }
+    }
+    public void printInt(int count){
+        String  s = "";
+        s += count;
+        System.out.println(s);
     }
 }
