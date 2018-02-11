3b3fc89d7f138074cb92d05abecdace3551cdd63
Carolynn Vansant <cvansant@MacBook-Pro-5.fios-router.home>
2018-02-10 21:08:26 -0500

    first and hopefully final commit

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,80 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    //constructor
+    public IntegerDuplicateDeleter (Integer[] intArray) {
+        super(intArray);
+    }
+
+
+    private static int countDuplicates (Integer[] array, Integer value) {
+        int dupeCount = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(value.equals(array[i])) {
+                dupeCount++;
+            }
+        }
+        return dupeCount;
+    }
+
+
+
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+
+        Integer[] notYetDel = Arrays.copyOf(this.array, this.array.length);
+        String keepInteger = "";
+
+        for(Integer keepIt: notYetDel) {
+            int x = countDuplicates(notYetDel, keepIt);
+            if (x < maxNumberOfDuplications) {
+                keepInteger += keepIt + "/";
+            }
+
+        }
+        if(keepInteger.equals("")) {
+            keepInteger = "/";
+        }
+
+        String[] dupsRemoved = keepInteger.split("/");
+        Integer [] arrayReturned = new Integer [dupsRemoved.length];
+
+        for(int i = 0; i<arrayReturned.length; i++) {
+            arrayReturned[i] = Integer.parseInt(dupsRemoved[i]);
+        }
+
+        return arrayReturned;
+    }
+
+
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Integer [] myIntArray = Arrays.copyOf(this.array, this.array.length);
+        String keepNumb = "";
+
+        for(Integer it: myIntArray) {
+            int x = countDuplicates(myIntArray, it);
+            if (x != exactNumberOfDuplications) {
+                keepNumb += it + "/";
+            }
+        }
+        if(keepNumb.equals("")) {
+            keepNumb = "/";
+        }
+
+        String[] stringToInt = keepNumb.split("/");
+        Integer[] exactIntRemoved = new Integer [stringToInt.length];
+
+        for(int i = 0; i<exactIntRemoved.length; i++) {
+            exactIntRemoved[i] = Integer.parseInt(stringToInt[i]);
+        }
+
+        return exactIntRemoved;
+    }
+
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,65 @@
 package com.zipcodewilmington.looplabs;
 
+
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+
+
+    private static int countDuplicates (String[] array, String value) {
+        int dupeCount = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(value.equals(array[i])) {
+                    dupeCount++;
+            }
+        }
+        return dupeCount;
+    }
+
+
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+
+        String[] myArray = Arrays.copyOf(this.array, this.array.length);
+        String keepString = "";
+
+        for(String keepIt: myArray) {
+            int x = countDuplicates(myArray, keepIt);
+            if (x < maxNumberOfDuplications) {
+                keepString += keepIt + "/";
+            }
+
+        }
+        if(keepString.equals("")) {
+            keepString = "/";
+        }
+
+        return keepString.split("/");
+    }
+
+
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+
+        String[] newArray = Arrays.copyOf(this.array, this.array.length);
+        String minusDeletes = "";
+
+        for(int i = 0; i < newArray.length; i++) {
+            int x = countDuplicates(newArray, newArray[i]);
+            if (x != exactNumberOfDuplications) {
+                minusDeletes += newArray[i] + "/";
+            }
+        }
+
+        return minusDeletes.split("/");
+    }
 }
+
