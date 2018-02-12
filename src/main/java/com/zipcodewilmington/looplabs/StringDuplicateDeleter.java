b6eb8589dd506444889ac0fe57f6ebac5ccc75c1
April Rivera <aprilrivera@zipcoders-MBP-2.fios-router.home>
2018-02-11 20:18:48 -0500

    adding string, i know it looks ugly and redundant : (

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -9,13 +9,75 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
         super(intArray);
     }
 
+    public boolean elementOccursLessTimesThanMaxThreshhold(String elementBeingExamined, int maxNumberOfDuplications) {
+        int occurencesOfElement = 0;
+        for (String value : this.array) {
+            if (value.equals(elementBeingExamined)) {
+                occurencesOfElement++;
+            }
+        }
+        return occurencesOfElement < maxNumberOfDuplications;
+    }
+
+
+    public boolean elementOccursExactNumberGiven(String elementBeingExamined, int exactNumberOfDuplications) {
+        int occurencesOfElement = 0;
+        for (String value : this.array) {
+            if (value.equals(elementBeingExamined)) {
+                occurencesOfElement++;
+            }
+        }
+        return occurencesOfElement != exactNumberOfDuplications;
+    }
+
+
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        String[] finalArray = new String[this.array.length];
+        int iterator = 0;
+        for (String value : this.array) {
+            if (elementOccursLessTimesThanMaxThreshhold(value, maxNumberOfDuplications)) {
+                finalArray[iterator] = value;
+                iterator++;
+                System.out.println(value);
+            }
+        }
+        int countOfNotNullElements = 0;
+        for (String value : finalArray) {
+            if (value != null) {
+                countOfNotNullElements++;
+            }
+        }
+        System.out.println("NOT NULL ELEMNTS " + countOfNotNullElements);
+        String[] finalfinalArray = new String[countOfNotNullElements];
+        for (int i = 0; i < countOfNotNullElements; i++) {
+            finalfinalArray[i] = finalArray[i];
+        }
+        return finalfinalArray;
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        String[] finalArray = new String[this.array.length];
+        int iterator = 0;
+        for (String value : this.array) {
+            if (elementOccursExactNumberGiven(value, exactNumberOfDuplications)) {
+                finalArray[iterator] = value;
+                iterator++;
+                System.out.println(value);
+            }
+        }
+        int countOfNotNullElements = 0;
+        for (String value : finalArray) {
+            if (value != null) {
+                countOfNotNullElements++;
+            }
+        }
+        System.out.println("NOT NULL ELEMNTS " + countOfNotNullElements);
+        String[] finalfinalArray = new String[countOfNotNullElements];
+        for (int i = 0; i < countOfNotNullElements; i++) {
+            finalfinalArray[i] = finalArray[i];
+        }
+        return finalfinalArray;
     }
 }
