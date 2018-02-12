fa4fb9579da33a14c394fd43b6d2851c6fa69b36
Keith Brinker <keithbrinker@zipcoders-MacBook-Pro-2.local>
2018-02-12 14:05:53 -0500

    finished

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,16 +1,60 @@
 package com.zipcodewilmington.looplabs;
-
+import java.util.Arrays;
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
 
-StringBuilder duplicateKiller = new StringBuilder();
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+
+        Integer[] finalArray = new Integer[0];
+
+        for (int i = 0; i < this.array.length; i++) {
+            if (getNumberOfOccurences(this.array, this.array[i]) != exactNumberOfDuplications) {
+
+                int actualElements = finalArray.length;
+                finalArray = Arrays.copyOf(finalArray, finalArray.length + 1);
+                finalArray[actualElements] = this.array[i];
+            }
+        }
+return finalArray;
+
+    }
 
 
+    public static int getNumberOfOccurences(Integer[] inputArray, Integer value) {
+        int numOccurences = 0;
+
+        for (int arrayElement : inputArray) {
+            if (arrayElement == value) {
+                numOccurences++;
+            }
+
+        }
+        return numOccurences;
+    }
 
 
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Integer[] actualArray = new Integer[0];
 
+        for (int i = 0; i < this.array.length; i++) {
+
+            if (getNumberOfOccurences(this.array, this.array[i]) < maxNumberOfDuplications) {
+
+                int arrayElements = actualArray.length;
+                actualArray = Arrays.copyOf(actualArray, actualArray.length + 1);
+                actualArray[arrayElements] = this.array[i];
+            }
+        }
+        return actualArray;
+    }
 
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,67 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+
+
+
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications){
+
+        String[] noStringDupsArray = new String[0];
+
+        for (int i = 0; i <this.array.length; i++){
+
+            if(getNumberOfOccurences(this.array, this.array[i]) != exactNumberOfDuplications) {
+
+                int stringElements = noStringDupsArray.length;
+                noStringDupsArray = Arrays.copyOf(noStringDupsArray, noStringDupsArray.length +1);
+                noStringDupsArray[stringElements] = this.array[i];
+            }
+        }
+        return noStringDupsArray;
+    }
+
+
+    public static int getNumberOfOccurences(String[] inputArray, String value) {
+
+        int valueCounter = 0;
+
+        for(String arrayElement : inputArray){
+
+            if (arrayElement.equalsIgnoreCase(value)){
+                valueCounter++;
+            }
+        }
+        return valueCounter;
+    }
+
+    @Override
+
+    public String[] removeDuplicates(int maxNumberOfDuplications){
+        String[] maxDupsArray = new String[0];
+        for (int i =0; i<this.array.length; i++){
+
+            if (getNumberOfOccurences(this.array, this.array[i]) < maxNumberOfDuplications){
+
+                int arrayElements = maxDupsArray.length;
+                maxDupsArray = Arrays.copyOf(maxDupsArray, maxDupsArray.length+1);
+                maxDupsArray[arrayElements] = this.array[i];
+            }
+        }
+        return maxDupsArray;
+    }
+
+
+
 }
