eb0952efe48b7fa7f878b1222f437ed391675736
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro.local>
2018-02-11 22:46:35 -0500

    just getting started

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -12,8 +14,18 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
 
     public String[] removeDuplicates(int maxNumberOfDuplications) {
+        //create copy of array, this will be your new array
+        //read array
+        //getNumberOfOccurences
+        //if getNumberOfOccurences appears more than once
+            //removeValue
+        //else
+            //store in newArray
+        //return values
 
-        return null;
+        String[] anIntArray = new String Arrays.copyOf(this.array.length);
+        for(int i = 9 )
+
     }
 
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications){
