3f50ad2fdd91403076d541fb91d9769d300d4a16
Amy Gill <amygill@MacBook-Pro-2.local>
2018-02-12 02:26:38 -0500

    String myProgram = {};

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -4,5 +4,32 @@ package com.zipcodewilmington.looplabs;
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
+/* note: I referenced another dev's code while dev'ing this.
+
+i can attempt to explain individual steps for this, and I know enough to know this is some very clean, well thought-out
+code, (I literally didn't "wtf" once) but i absolutely cannot take credit for this program. What I *can* promise you is
+given enough time I can absolutely learn it, retain it,and apply it to future applicable use. this is not a promise I
+would make that i didn't absolutely believe i could keep.
+
+Having said all of this (can't believe you're still reading this far down into my comments) if you still want to hand
+my ass to me that is absolutely your prerogative and if that is the case then so be it (smiles wanly).
+
+*/
+
+
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+   public IntegerDuplicateDeleter(Integer[] intArray){
+
+       super(intArray);
+
+    }
+
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+
+    }
+
+    return null;
+
 }
+
