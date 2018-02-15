7d313ab776140a65c8bcd3e5c324f6528589a129
Jordan Elderidge <jordanelderidge@zipcoders-MacBook-Pro.local>
2018-02-15 17:22:25 -0500

    done

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,57 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Integer[] removeDupExact = new Integer[0];
+        for (int i =0; i < this.array.length; i++){
+            if (getNumberOfOccurrences(this.array,this.array[i]) < maxNumberOfDuplications){
+                int element = removeDupExact.length;
+                removeDupExact = Arrays.copyOf(removeDupExact,removeDupExact.length +1);
+                removeDupExact[element] = this.array[i];
+            }
+
+        }
+
+        return removeDupExact;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Integer[] removeDupExact = new Integer[0];
+        for (int i =0; i < this.array.length; i++){
+            if (getNumberOfOccurrences(this.array,this.array[i])!= exactNumberOfDuplications){
+                int element = removeDupExact.length;
+                removeDupExact = Arrays.copyOf(removeDupExact,removeDupExact.length +1);
+                removeDupExact[element] = this.array[i];
+            }
+
+        }
+
+        return removeDupExact;
+
+
+    }
+    public int getNumberOfOccurrences (Integer[]array,Integer value) {
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(value)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,58 @@
 package com.zipcodewilmington.looplabs;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        String[] removeDupExact = new String[0];
+        for (int i =0; i < this.array.length; i++){
+            if (getNumberOfOccurrences(this.array,this.array[i]) < maxNumberOfDuplications){
+                int element = removeDupExact.length;
+                removeDupExact = Arrays.copyOf(removeDupExact,removeDupExact.length +1);
+                removeDupExact[element] = this.array[i];
+            }
+
+        }
+
+        return removeDupExact;
+
+
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        String[] removeDupExact = new String[0];
+        for (int i =0; i < this.array.length; i++){
+            if (getNumberOfOccurrences(this.array,this.array[i])!= exactNumberOfDuplications){
+                int element = removeDupExact.length;
+                removeDupExact = Arrays.copyOf(removeDupExact,removeDupExact.length +1);
+                removeDupExact[element] = this.array[i];
+            }
+
+        }
+
+        return removeDupExact;
+    }
+    public int getNumberOfOccurrences (String[]array,String value) {
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(value)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
+
