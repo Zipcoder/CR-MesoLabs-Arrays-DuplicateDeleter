652309e3ab520dc45ebcf188ec1c776964b55c3e
wes <wes.jones1597@gmail.com>
2019-11-15 09:53:54 -0500

    IntegerDuplicateDeleter tests passing

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.looplabs;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -10,10 +13,32 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
     }
 
     public Integer[] removeDuplicates(int maxNumberOfDuplications) {
-        return null;
+        ArrayList<Integer> newList = new ArrayList<>();
+        for (Integer num : array) {
+            if (getNumberOfOccurrances(num) < maxNumberOfDuplications) {
+                newList.add(num);
+            }
+        }
+        return newList.toArray(new Integer[0]);
+    }
+
+    public Integer getNumberOfOccurrances(Integer element) {
+        Integer count = 0;
+        for (Integer num : array) {
+            if (num.equals(element)) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return null;
+        ArrayList<Integer> newList = new ArrayList<>();
+        for (Integer num : array) {
+            if (getNumberOfOccurrances(num) != exactNumberOfDuplications) {
+                newList.add(num);
+            }
+        }
+        return newList.toArray(new Integer[0]);
     }
 }
