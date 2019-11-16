7e1bb9a465aefe5d3d6e11d2e1aac986893513d3
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-16 17:08:26 -0500

    finished integer

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -7,10 +7,11 @@ package com.zipcodewilmington.looplabs;
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
 
-    private Integer[] result;
-    private int counter = 1;
+
+    private int counter = 0;
     private int index = 0;
     private int newArrayLength = 0;
+
 
     public IntegerDuplicateDeleter(Integer[] intArray) {
         super(intArray);
@@ -18,45 +19,88 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
 
     @Override
     public Integer[] removeDuplicates(int maxNumberOfDuplications) {
-        return new Integer[0];
-    }
-
-    @Override
-    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Integer[] interimArray = new Integer[array.length];
+        System.arraycopy(array,0,interimArray,0,array.length);
+        newArrayLength = 0;
         Integer [] result;
+        int temp = 0;
 
-        for (int i = 0; i < array.length -1; i++) {
-            if (array[i] == array[i + 1]) {
-                counter++;
-                if (counter == exactNumberOfDuplications) {
-                    newArrayLength += counter;
-                    while (counter > 0) {
-                        index = i + 2 - counter;
-                        array[index] = null;
-                        counter--;
+        for (int i = 0; i < interimArray.length -1; i++) {
+            counter = 0;
+            checkForDuplicates(interimArray[i], interimArray);
+
+            if (counter >= maxNumberOfDuplications && interimArray[i] != null) {
+                counter = interimArray.length -1;
+                temp = interimArray[i];
+                while (counter >= 0) {
+                    if (interimArray[counter] != null) {
+                        if (interimArray[counter] == temp) interimArray[counter] = null;
                     }
+                    counter--;
                 }
-            } else counter = 1;
-
+            }
         }
 
-        result = buildNewArray();
+        getNewArrayLength(interimArray);
+        result = buildNewArray(interimArray);
 
         if (result.length == array.length) return array; // no changes
 
         return result;
     }
 
-    private Integer[] buildNewArray(){
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Integer[] interimArray = new Integer[array.length];
+        System.arraycopy(array,0,interimArray,0,array.length);
+        Integer [] result;
+        newArrayLength = 0;
+        for (int i = 0; i < array.length -1; i++) {
+            counter = 0;
+            checkForDuplicates(interimArray[i],interimArray);
+
+                if (counter == exactNumberOfDuplications && interimArray[i] != null) {
+                    while (counter > 0) {
+                        index = i + exactNumberOfDuplications - counter;
+                        interimArray[index] = null;
+                        counter--;
+                    }
+                }
+        }
+
+        getNewArrayLength(interimArray);
+        result = buildNewArray(interimArray);
+
+        if (result.length == array.length) return array; // no changes
+
+        return result;
+    }
+
+    private Integer[] buildNewArray(Integer [] arrayNew){
         Integer [] temp = new Integer[array.length - newArrayLength];
         index = 0;
         for (int i = 0; i < array.length; i++){
-            if (array[i] != null){
-                temp[index] = array[i];
+            if (arrayNew[i] != null){
+                temp[index] = arrayNew[i];
                 index++;
             }
         }
         return temp;
+    }
+
+    private void checkForDuplicates (Integer element, Integer [] arrayNew){
+        for (Integer eachElement : arrayNew) {
+            if (eachElement == element && eachElement != null) {
+                counter++;
+            }
+        }
+    }
+
+    private void getNewArrayLength(Integer [] arrayNew) {
+        for (Integer eachElement : arrayNew) {
+            if (eachElement == null) newArrayLength ++;
+        }
+
     }
 
 
