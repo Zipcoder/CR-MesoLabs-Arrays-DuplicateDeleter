ea111da0f050d9a700e532b8cc9805cfffc7ffd7
vaidehi4321 <56602405+vaidehi4321@users.noreply.github.com>
2019-11-18 13:31:50 -0500

    integer deleter done

--- src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
@@ -11,5 +11,7 @@ public abstract class DuplicateDeleter<T> implements DuplicateDeleterInterface<T
     }
 
     abstract public T[] removeDuplicates(int maxNumberOfDuplications);
+
+
     abstract public T[] removeDuplicatesExactly(int exactNumberOfDuplications);
 }
--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,94 @@
 package com.zipcodewilmington.looplabs;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by leon on 1/29/18.
+ *
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+
+
+    }
+
+
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        int k=0;
+        Integer[]arr=new Integer[array.length];
+        for(int i= 0;i<array.length;i++){
+       if(getOccurences(array[i])<maxNumberOfDuplications){
+          arr[k]=array[i];
+          k++;
+          }
+
+
+       }
+        int counterNull=0;
+        for(int i=0;i<arr.length;i++){
+       if(arr[i]==null){
+           counterNull++;
+       }
+
+}
+        Integer[] filtered=new Integer[arr.length-counterNull];
+        int m=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=null){
+                filtered[m]=arr[i];
+                m++;
+            }
+        }
+        System.out.println(filtered);
+        return  filtered;
+    }
+
+
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int k=0;
+        Integer[]arr=new Integer[array.length];
+        for(int i= 0;i<array.length;i++){
+            if(getOccurences(array[i])!=exactNumberOfDuplications){
+                arr[k]=array[i];
+                k++;
+            }
+
+
+        }
+        int counterNull=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==null){
+                counterNull++;
+            }
+
+        }
+        Integer[] filtered=new Integer[arr.length-counterNull];
+        int m=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=null){
+                filtered[m]=arr[i];
+                m++;
+            }
+        }
+        System.out.println(filtered);
+        return  filtered;
+    }
+
+
+
+
+
+    public int getOccurences( int objectToCount) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == objectToCount) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
 }
